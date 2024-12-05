dc6b41368f792cd2421196ff5da0859c770bc2b6
saimanion <97195306+saimanion@users.noreply.github.com>
2024-12-05 11:48:44 +0100

    Initialize project using Create React App

---
+++ main.js
@@ -0,0 +1,90 @@
+const { ethers } = require("ethers");
+// Подключаемся к локальной сети Ganache
+const provider = new ethers.JsonRpcProvider("http://127.0.0.1:7545");
+// Приватный ключ одного из аккаунтов Ganache
+const privateKey = "0x256bbbcde1994cba30982873a4d4667eeac72ba2349163eb0f8bb54e0dfda582"; // Вставьте сюда ключ из Ganache
+const wallet = new ethers.Wallet(privateKey, provider);
+
+const contractAddress = "0x24254CAcCB02Fc0eca24B3643bBA5427e28654D4"; // Адрес вашего развернутого контракта
+const abi = [
+    {
+        "inputs": [],
+        "stateMutability": "nonpayable",
+        "type": "constructor"
+    },
+    {
+        "inputs": [],
+        "name": "owner",
+        "outputs": [
+            {
+                "internalType": "address",
+                "name": "",
+                "type": "address"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "payForItem",
+        "outputs": [],
+        "stateMutability": "payable",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "address",
+                "name": "",
+                "type": "address"
+            }
+        ],
+        "name": "payments",
+        "outputs": [
+            {
+                "internalType": "uint256",
+                "name": "",
+                "type": "uint256"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "withdrawAll",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    }
+];
+
+const contract = new ethers.Contract(contractAddress, abi, wallet);
+const contractWalletAddress = wallet.address;
+
+async function getBalance() {
+    try {
+        // Получаем баланс
+        const balance = await provider.getBalance(contractWalletAddress);
+        // Форматируем баланс в эфиры и выводим
+        const formattedBalance = ethers.formatEther(balance)
+        console.log(ethers.formatEther(balance));
+        console.log(balance)
+        return formattedBalance
+    } catch (error) {
+        console.error("Ошибка при получении баланса:", error);
+    }
+}
+async function getInfo() {
+    try {
+        const balanceInEther = await getBalance()
+        console.log(`Ваш баланс в ETH: ${balanceInEther}`);
+    } catch (error) {
+        console.log('ошибка при получении информации')
+    }
+}
+
+// getBalance();
+// console.log()
+getInfo()
